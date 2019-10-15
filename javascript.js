40a527760f9c3f8e7afbc72eeef74215ffcfe6d0
ricou <e.lanza@free.fr>
2019-10-15 22:09:51 +0200

    image modal modifié

--- javascript.js
+++ javascript.js
@@ -57,7 +57,7 @@ var myDataBase = [{
         'titre': 'Le jardin botanique de Papeari (Tahiti)',
         'commentaires': "<blockquote>Si Papeari est renommé pour ses fleurs et son jardin botanique, c’est en grande partie grâce à un américain, Harrison Willard Smith (1872 1947), passionné par la botanique, que les Tahitiens appelaient « le grand-père des arbres ».<br><br>L’américain Harrison Willard Smith arrive à Tahiti en 1919, à l’âge de 37 ans, où il savait d’une précédente visite, qu’il trouverait à Papeari un sol et un climat idéal pour créer le jardin tropical dont il rêvait. Il acheta, de 1920 à 1925, les propriétés sur lesquelles il devait créer le domaine de Motu-Ovini (l’îlot sauvage) de 137 hectares où il planta petit à petit des centaines de variétés de fleurs et d’arbres exotiques qu’il faisait venir à grands frais et avec bien des difficultés des régions tropicales d’Amérique, d’Asie et d’Afrique. Un grand nombre des arbres et des plantes de ce jardin botanique sont utilitaires et ont été introduits par Harrison Smith.</blockquote>",
         'dossier_photo': 'img/HarrisonSmith/',
-        'files': ['1.JPG', '2.JPG', '3.JPG', '4.JPG', '5.JPG', '6.JPG', '7.JPG', '8.JPG', '9.JPG', '10.JPG', '11.JPG', '12.JPG', '13.JPG', '14.JPG', '15.JPG', '16.JPG', '17.JPG', '18.JPG', '19.JPG', '20.JPG'],
+        'files': ['1.JPG', '2.JPG', '3.JPG', '4.JPG', '5.JPG', '6.JPG', '7.JPG', '8.JPG', '9.JPG', '10.JPG', '11.JPG', '12.JPG', '13.JPG', '14.JPG', '15.JPG', '16.JPG', '17.JPG', '18.JPG', '19.JPG'],
     },
     {
         'identifiant': 'moorea',
